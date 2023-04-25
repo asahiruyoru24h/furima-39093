1edc1d45a017b6a0a515e04118d18b7f6caf3d5f
asahiruyoru24h <kakimiekaki@gmail.com>
2023-04-25 21:02:32 +0900

    文字数修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -112,7 +112,7 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password is invalid")
     end
     it 'passwordが数字のみでは登録できない' do
-      @user.password = '00000'
+      @user.password = '000000'
       @user.password_confirmation = '000000'
       @user.valid?
       expect(@user.errors.full_messages).to include("Password is invalid")
