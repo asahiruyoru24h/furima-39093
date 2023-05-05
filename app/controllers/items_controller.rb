9150867199a20b2f4d06f546e4b40da1e133da1f
asahiruyoru24h <kakimiekaki@gmail.com>
2023-05-05 17:07:49 +0900

    簡略化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_tweet, only: [:edit, :show, :update]
 
   def index
 
@@ -22,13 +23,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
 
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user == @item.user
 
   end
@@ -37,7 +36,6 @@ class ItemsController < ApplicationController
 
 
   def update
-    @item = Item.find(params[:id])
    if @item.update(item_params)
       redirect_to item_path
     else
@@ -59,5 +57,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :about, :prepare_id, :category_id, :condition_id, :image, :shipment_fee_id, :price, :place_id).merge(user_id: current_user.id)
   end
 
- 
+  def set_tweet
+    @item = Item.find(params[:id])
+  end
 end
