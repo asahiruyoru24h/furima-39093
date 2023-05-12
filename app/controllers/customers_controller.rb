7372bba30d545ed5f070b8e4bf0533201dc36280
asahiruyoru24h <kakimiekaki@gmail.com>
2023-05-12 09:23:02 +0900

    修正

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,19 @@
 class CustomersController < ApplicationController
 
+  before_action :move_to_signed_in, only: [:index]
+
+
 
   def index
     @item = Item.find(params[:item_id])
     @address_customer = AddressCustomer.new
+    
+    if current_user == @item.user
+       redirect_to root_path
+      end
+    if @item.customer.present? 
+      redirect_to root_path
+    end
   end
 
 def create
@@ -23,6 +33,15 @@ def customer_params
   params.require(:address_customer).permit(:postcode, :place_id, :city, :address_line, :building, :mobile_number).merge(user_id: current_user.id, item_id: @item.id)
 
 end
+
+
+def move_to_signed_in
+  unless user_signed_in?
+    redirect_to root_path 
+  end
+
+end
+
 
 end
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,9 @@ class ItemsController < ApplicationController
 
 
   def edit
-
+    if current_user == @item.user
+      redirect_to root_path
+     end
   end
 
 
