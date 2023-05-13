fc0cddc7b559aaa90eb78fb4fb9cbc1c24350059
asahiruyoru24h <kakimiekaki@gmail.com>
2023-05-13 16:59:49 +0900

    修正

--- app/models/address_customer.rb
+++ app/models/address_customer.rb
@@ -16,13 +16,13 @@ class AddressCustomer
     validates :user_id
     validates :mobile_number, format: {with: /\A\d{10,11}\z/ }
     validates :address_line
-    validates :token, presence: true
+    validates :token
   end
 
   def save
     customer = Customer.create(item_id: item_id, user_id: user_id)
    
-    Address.create(postcode: postcode, place_id: place_id, city: city, address_line: address_line, building: building, mobile_number: mobile_number, customer_id: customer_id)
+    Address.create(postcode: postcode, place_id: place_id, city: city, address_line: address_line, building: building, mobile_number: mobile_number, customer_id: customer.id)
  
 
   end
